45cc98216e381a7a00c7235c0ccabcb888800841
MhdxMx <mhdxmx694@gmail.com>
2025-12-21 00:39:16 +0300

    first

--- js/guard.js
+++
@@ -1,8 +0,0 @@
-// Redirect to login.html if user hasn't unlocked the card yet
-(function () {
-    if (window.location.pathname.endsWith("login.html")) return; // avoid loop
-    var allowed = localStorage.getItem("birthdayLogin") === "true";
-    if (!allowed) {
-        window.location.href = "login.html";
-    }
-})();
